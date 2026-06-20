66d4267932a7fe864c46c6904c514a85f1185f05
aliyazdanpanah-beep <aliyazdanpanahfard@gmail.com>
2026-06-20 19:58:59 +0330

    update

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from router import auth, admin, user, product
+from router import auth, admin, user, product, cart
 from models import Base
 from database import engine
 
@@ -11,3 +11,4 @@ app.include_router(product.router)
 app.include_router(auth.router)
 app.include_router(admin.router)
 app.include_router(user.router)
+app.include_router(cart.router)
