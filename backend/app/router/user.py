fd0e0a27278bd279f459dd103d0ebf13d4acfc5e
aliyazdanpanah-beep <aliyazdanpanahfard@gmail.com>
2026-06-25 22:37:47 +0330

    adding dependecis file for get_db function and bcypt context

--- backend/app/router/user.py
+++ backend/app/router/user.py
@@ -1,12 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException, Path, Query
+from fastapi import APIRouter, Depends, HTTPException
 from models import Users
-from database import SessionLocal
-from sqlalchemy.orm import Session
 from typing import Annotated
 from starlette import status
 from pydantic import BaseModel, Field
 from .auth import get_current_user
-from passlib.context import CryptContext
+from dependencies import db_dependency, bcrypt_context
 
 
 router = APIRouter(
@@ -15,37 +13,29 @@ router = APIRouter(
 )
 
 
-def get_db():
-     db = SessionLocal()
-     try:
-          yield db
-     finally:
-          db.close()
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 
 class user_verfic(BaseModel):
-     password: str
-     new_password: str = Field(min_length=6)
-  
-
-db_dependency = Annotated[Session, Depends(get_db)]
-user_dependency = Annotated[dict, Depends(get_current_user)]
-
-bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+    password: str
+    new_password: str = Field(min_length=6)
 
 @router.get('/', status_code=status.HTTP_200_OK)
 async def get_first_user(user: user_dependency, db: db_dependency):
      if user is None:
-          raise HTTPException(status_code=401, detail='Authentication Filed')
+          raise HTTPException(status_code=401, detail='Authentication Failed')
      return db.query(Users).filter(Users.id == user.get('id')).first()
 
 
-@router.put('/password/}', status_code=status.HTTP_200_OK)
+@router.put('/password/', status_code=status.HTTP_200_OK)
 async def change_password(user: user_dependency, db: db_dependency, verifay: user_verfic):
      if user is None:
-          raise HTTPException(status_code=401, detail='Authentication Filed')
+          raise HTTPException(status_code=401, detail='Authentication Failed')
      user_model = db.query(Users).filter(Users.id == user.get('id')).first()
-     
+    
+     if user_model is None:
+         raise HTTPException(status_code=404, detail='User not found')
+    
      if not bcrypt_context.verify(verifay.password, user_model.hashed_password):
           raise HTTPException(status_code=401, detail="Error on password change")
      user_model.hashed_password = bcrypt_context.hash(verifay.new_password)
